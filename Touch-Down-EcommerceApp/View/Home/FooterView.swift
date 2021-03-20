661100953e458f8bee378c7216b51a89519b3dfc
Ugozeal <okonkwodavidu@gmail.com>
2021-03-20 14:02:32 +0100

    create a footer view for the app

--- Touch-Down-EcommerceApp/View/Home/FooterView.swift
+++ Touch-Down-EcommerceApp/View/Home/FooterView.swift
@@ -9,12 +9,31 @@ import SwiftUI
 
 struct FooterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .center, spacing: 10){
+            Text("We offer the most cutting edge, comfortable, light-weight and durable football helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © David Okonkwo\nAll rights reserved.")
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        }//:VSTACK
+        .padding()
     }
 }
 
 struct FooterView_Previews: PreviewProvider {
     static var previews: some View {
         FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(backgroundColor)
     }
 }
