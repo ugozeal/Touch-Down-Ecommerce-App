70b64704accf6ec57bb3f046f66a92b9e24422d9
Ugozeal <okonkwodavidu@gmail.com>
2021-03-20 13:30:51 +0100

    setup the project using swiftui

---
+++ Touch-Down-EcommerceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touch-Down-EcommerceApp/Touch_Down_EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Touch_Down_EcommerceAppApp.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Touch_Down_EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
