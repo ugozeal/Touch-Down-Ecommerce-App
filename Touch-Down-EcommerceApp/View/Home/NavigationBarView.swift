5978f17524f0fa9fa8a0f20de8700eff91fcb124
Ugozeal <okonkwodavidu@gmail.com>
2021-03-20 14:21:54 +0100

    created  custom navigation view and added the logoview in to it

--- Touch-Down-EcommerceApp/View/Home/NavigationBarView.swift
+++ Touch-Down-EcommerceApp/View/Home/NavigationBarView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct NavigationBarView: View {
+     //MARK: - PROPERTIES
+    @State var isAnimated: Bool = false
+    
+     //MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {}) {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }//: BUTTON
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25 )
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+            
+            Spacer()
+            Button(action: {}) {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            }//: BUTTON
+        }//: HSTACK
     }
 }
 
+ //MARK: - PREVIEW
 struct NavigationBarView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
