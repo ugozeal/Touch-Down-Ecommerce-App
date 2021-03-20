64f8c4924fd655a39f9955667ad05f64eaf08b64
Ugozeal <okonkwodavidu@gmail.com>
2021-03-20 14:01:56 +0100

    create a constant file to hold reusable constants

---
+++ Touch-Down-EcommerceApp/Utility/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/20/21.
+//
+
+import SwiftUI
+
+//DATA
+//COLOR
+let backgroundColor: Color = Color("ColorBackground")
+let grayColor: Color = Color(UIColor.systemGray4 )
+
+//LAYOUT
+//UX
+//API
+//IMAGE
+//FONT
+//STRING
+//MISC
+
---
+++ Touch-Down-EcommerceApp/View/Home/FooterView.swift
@@ -0,0 +1,20 @@
+//
+//  FooterView.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/20/21.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
