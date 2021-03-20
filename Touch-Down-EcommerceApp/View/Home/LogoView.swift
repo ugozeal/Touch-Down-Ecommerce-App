5b844475d1255c474a0d46501c1e0b3314c201e6
Ugozeal <okonkwodavidu@gmail.com>
2021-03-20 14:21:20 +0100

    created a custom logoview

---
+++ Touch-Down-EcommerceApp/View/Home/LogoView.swift
@@ -0,0 +1,36 @@
+//
+//  LogoView.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/20/21.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }//: HStack
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
---
+++ Touch-Down-EcommerceApp/View/Home/NavigationBarView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBarView.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/20/21.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+    }
+}
