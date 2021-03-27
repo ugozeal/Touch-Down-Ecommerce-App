464b93ebcf8d350d020e78158c8f97540f1e43e1
Ugozeal <okonkwodavidu@gmail.com>
2021-03-27 18:58:19 +0100

    created a model for the player

---
+++ Touch-Down-EcommerceApp/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touch-Down-EcommerceApp
+//
+//  Created by David U. Okonkwo on 3/27/21.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
